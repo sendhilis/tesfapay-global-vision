49878c77f5c61bcfeee1ea1dc5b967d5726b3cca
sendhilis <sendhilis@gmail.com>
2026-03-27 14:48:17 +0530

    feat: SecurityConfig.java

---
+++ globalpay/gp-agent-service/src/main/java/com/globalpay/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.globalpay.config;
+import com.globalpay.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Configuration @On @EnableWebSecurity @EnableMethodSecurity @RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth->auth.requestMatchers("/actuator/**","/api-docs/**","/swagger-ui/**", "/agents/nearby").permitAll().anyRequest().authenticated())
+            .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class).build();
+    }
+}
