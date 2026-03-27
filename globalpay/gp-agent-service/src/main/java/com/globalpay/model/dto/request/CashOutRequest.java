1bd52493670596191b445120e7d93a8589e605b6
sendhilis <sendhilis@gmail.com>
2026-03-27 13:49:18 +0530

    feat: CashOutRequest.java

---
+++ globalpay/gp-agent-service/src/main/java/com/globalpay/model/dto/request/CashOutRequest.java
@@ -0,0 +1,5 @@
+package com.globalpay.model.dto.request;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.math.BigDecimal;
+@Data public class CashOutRequest { @NotBlank private String customerPhone; @NotNull @DecimalMin("10.00") private BigDecimal amount; @NotBlank private String otp; }
