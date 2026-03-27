109194e5336326d14423e8fb3e738913fabe629c
sendhilis <sendhilis@gmail.com>
2026-03-27 13:49:15 +0530

    feat: CashInRequest.java

---
+++ globalpay/gp-agent-service/src/main/java/com/globalpay/model/dto/request/CashInRequest.java
@@ -0,0 +1,5 @@
+package com.globalpay.model.dto.request;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.math.BigDecimal;
+@Data public class CashInRequest { @NotBlank private String customerPhone; @NotNull @DecimalMin("10.00") private BigDecimal amount; @NotBlank private String otp; @NotBlank private String agentPin; }
