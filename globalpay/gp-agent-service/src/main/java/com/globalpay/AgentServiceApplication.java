2b30f3d2ee8e4ddcb33c2a767934952eb1bcaa41
sendhilis <sendhilis@gmail.com>
2026-03-27 13:45:41 +0530

    feat: AgentServiceApplication.java

---
+++ globalpay/gp-agent-service/src/main/java/com/globalpay/AgentServiceApplication.java
@@ -0,0 +1,9 @@
+package com.globalpay;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@SpringBootApplication @EnableDiscoveryClient @EnableFeignClients
+public class AgentServiceApplication {
+    public static void main(String[] args) { SpringApplication.run(AgentServiceApplication.class, args); }
+}
